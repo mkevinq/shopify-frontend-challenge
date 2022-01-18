1996e95f8d4c241a3c0d131e7b031bd6cdfbd3d5
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2022-01-18 17:58:58 -0500

    add github link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,6 +8,7 @@ import {
 } from "react";
 
 import Calendar from "../public/calendar.svg";
+import GitHub from "../public/github.svg";
 import Heart from "../public/heart.svg";
 import Home from "../public/home.svg";
 import Moon from "../public/moon.svg";
@@ -26,6 +27,7 @@ type LayoutProps = {
  */
 const Layout: FunctionComponent<LayoutProps> = ({ title, children }) => {
   const [dark, setDarkMode] = useState(false);
+  const githubLink = useRef<HTMLAnchorElement>(null);
   const homeLink = useRef<HTMLAnchorElement>(null);
   const likedLink = useRef<HTMLAnchorElement>(null);
   const dateLink = useRef<HTMLAnchorElement>(null);
@@ -43,6 +45,25 @@ const Layout: FunctionComponent<LayoutProps> = ({ title, children }) => {
         id="header"
         className="sticky top-0 w-full h-12 p-4 flex place-items-center justify-between z-10 bg-white dark:bg-neutral-700 dark:text-white drop-shadow-md"
       >
+        <Link
+          passHref={true}
+          href="https://github.com/mkevinq/shopify-frontend-challenge"
+        >
+          <a
+            id="github-link"
+            tabIndex={0}
+            ref={githubLink}
+            onKeyDown={() => githubLink.current?.click()}
+            className="flex flex-col place-items-center text-xs"
+          >
+            <GitHub
+              role="img"
+              title="GitHub"
+              alt="GitHub logo"
+              className="h-6 w-6"
+            />
+          </a>
+        </Link>
         <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-purple-500">
           Spacetagram
         </h1>
